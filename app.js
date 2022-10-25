3fa5e9fa01764f328fb5cfedfd0681aa0830e748
joe <67911762+saberzap@users.noreply.github.com>
2022-10-25 14:55:17 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+const number = Math.floor((Math.random(0, 1) * 100) + 1);
+const heading = document.getElementsByTagName('h3')[0]
+const form = document.getElementsByTagName('form')[0]
+const guess = document.getElementById('guess-input');
+const userGuesses = document.getElementById('user-guesses');
+const guessFeedback = document.getElementsByTagName('h1')[0];
+
+function getGuess(e) {
+    e.preventDefault()
+    
+    let userGuess = guess.value;
+    
+    userGuesses.innerHTML += ` ${userGuess}`
+    handleGuess(userGuess)
+    guess.value = "";
+
+}
+
+function handleGuess(userGuess) {
+    if(userGuess < number) {
+        guessFeedback.innerHTML = "Your guess is too low. Guess higher!";
+    }
+    else if(userGuess > number) {
+        guessFeedback.innerHTML = "Your guess is too high. Guess lower!";
+    }
+    else {
+        guessFeedback.innerHTML = "You guessed correctly. Game over!";
+        endGame();
+    }
+}
+
+function endGame(){ 
+    document.getElementsByTagName('body')[0].removeChild(form)
+    document.getElementsByTagName('body')[0].removeChild(heading)
+
+    guessFeedback.innerHTML = `Random number: ${number}`
+}
+
+
